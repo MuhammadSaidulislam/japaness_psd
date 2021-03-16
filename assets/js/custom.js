1dd0ae5e776a223a67e9d596c2fbfed01fdcb12b
Saidul Islam <mislam171222@bscse.uiu.ac.bd>
2021-03-16 23:52:15 +0600

    half done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,40 +1,93 @@
-// sticky navbar
-window.onscroll = function () { myFunction() };
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
+// navbar setting
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbarSticky").style.display = "none";
+    document.getElementById("navbarStickyTwo").style.display = "block";
   } else {
-    navbar.classList.remove("sticky");
+     document.getElementById("navbarSticky").style.display = "block";
+     document.getElementById("navbarStickyTwo").style.display = "none";
   }
 }
 
+// aos slider
+AOS.init();
+
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 2000, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+
+
 // slider carousel
 $('.owl-carousel').owlCarousel({
-  stagePadding: 120,
-  items: 4,
+  items: 7,
+  loop: true,
+  nav: false,
+  margin: 10,
+  autoplay: true,
+  autoplayTimeout: 1000,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 5,
+
+    },
+    1000: {
+      items: 7,
+    }
+  }
+})
+
+
+
+// slider carousel
+$('.owl-carousel').owlCarousel({
+  items: 7,
   loop: true,
   nav: true,
   margin: 10,
   autoplay: true,
   autoplayTimeout: 1000,
   autoplayHoverPause: true,
-  navText: ["<div class='nav-button owl-prev'>‹</div>", "<div class='nav-button owl-next'>›</div>"],
+  navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
   responsive: {
     0: {
       items: 1,
-      stagePadding: 50,
-
     },
     600: {
-      items: 2,
+      items: 5,
 
     },
     1000: {
-      items: 2,
+      items: 7,
     }
   }
+  
 })
 // collapse course js
 
